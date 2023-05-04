12fe2f109b6e448f754fcc4fd98e71c985e7a2a3
susnfor <97429786+Susnfor@users.noreply.github.com>
2023-05-04 17:35:34 +0100

    mobile responsive

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,7 +4,7 @@ function Welcome(props) {
 
     return (
         <div className="App-top">
-        <header className="App-header">Welcome {props.name}</header>
+        <header className="App-header">Welcome <span style={{fontWeight: "bold"}}>{props.name}</span></header>
         <p>If you're relying on your motivation to keep you focused, you're going to be in a perpetual state of lateness and last-minute-ness because there's never enough motivation to go around</p>
         </div>
     );
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,15 +1,13 @@
 import '../styles/App.css';
-import { BrowserRouter as Router, HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import AnimatedRoutes from '../components/AnimatedRoutes';
 
 
 function App() {
   return (
-    // <Router basename='/'>
        <HashRouter basename="/">
         <AnimatedRoutes/>
        </HashRouter>
-    // </Router>
   );
 }
 
